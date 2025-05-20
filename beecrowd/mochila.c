a30ce8832a91db2a0f5c6d0938006b7b0f410fd0
matheusd-ramos <matheusd.araujo5@gmail.com>
2025-05-19 23:19:42 -0300

    Alterações

--- beecrowd/mochila.c
+++ beecrowd/mochila.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-void solucoes_otimas(){
+void solucoes_otimas(int pesos[], int cap, int N, int ***matriz){
     
+}
+
+void criaMatriz(int cap, int N, int ***matriz){                 // Passar endereço da matriz como parâmetro
+
+    *matriz = (int**)calloc(N+1, sizeof(int*));                // Aloca N+1 arrays de inteiros (linhas)
+
+    for(int i=0; i<=N; i++){
+        (*matriz)[i] = (int *)calloc(cap+1, sizeof(int));       // Aloca cap+1 espaços em cada array (colunas)
+    }
 }
 
 int main(){
 
     int cap, N, pesoT = 0;
+    int **matriz;
+
     scanf("%d\n%d", &cap, &N);
 
     int pesos[N], valores[N], escolhidos[N];
+    criaMatriz(cap, N, &matriz);
 
     for(int i = 0; i < N; i++){
         scanf("%d %d", &pesos[i], &valores[i]);
@@ -27,7 +39,11 @@ int main(){
         return 0;
     }
 
-
+    // Liberando a memória
+    for (int i = 0; i <= N; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
 
     return  0;
 }
