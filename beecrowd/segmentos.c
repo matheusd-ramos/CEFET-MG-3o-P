f030519df02ebc0423e89ab1937b06c6d2521748
matheusd-ramos <matheusd.araujo5@gmail.com>
2025-06-04 09:54:26 -0300

    Exercício finalizado

--- beecrowd/segmentos.c
+++ beecrowd/segmentos.c
@@ -17,19 +17,18 @@ void ordenaVetor(int vetor[], int N, int *vetorOrdenado){
     }
 }
 
-int* simplificaVetor(int *vetorOrdenado, int N, int *tam) {
-    int *vetorSimplificado = malloc(sizeof(int) * N); 
+void simplificaVetor(int *vetorOrdenado, int **vetorSimplificado, int *tam, int N) {
+    *vetorSimplificado = malloc(sizeof(int) * N); 
     *tam = 0;
 
     for (int i = 0; i < N; i++) {
         if (i == 0 || vetorOrdenado[i] != vetorOrdenado[i - 1]) {
-            vetorSimplificado[*tam] = vetorOrdenado[i];
+            (*vetorSimplificado)[*tam] = vetorOrdenado[i];
             (*tam)++;
         }
     }
 
-    vetorSimplificado = realloc(vetorSimplificado, (*tam) * sizeof(int));
-    return vetorSimplificado;
+    *vetorSimplificado = realloc(*vetorSimplificado, (*tam) * sizeof(int));
 }
 
 int contaSegmentos(int *vetorOrdenado, int N){
@@ -48,19 +47,48 @@ int buscaSegmentoDoNum(int *vetorSimplificado, int num, int tam){
     }
 }
 
-int* criaVetorSegmentos(int vetor[], int *vetorSimplificado, int tam, int N){
-    int *vetorSegmentos = malloc(N*sizeof(int));
-    int tamVetorSegmentos = 1;
+void criaVetorSegmentos(int vetor[], int *vetorSimplificado, int **vetorSegmentos, int tam, int N){
+    *vetorSegmentos = malloc(N*sizeof(int));
+    int tamVetorSegmentos = 0;
 
-    for(int i=0; i<N-1; i++){
-        if(vetor[i] != vetor[i+1]){
-            vetorSegmentos[tamVetorSegmentos-1] = buscaSegmentoDoNum(vetorSimplificado, vetor[i], tam);
+    for(int i=0; i<N; i++){
+        if(i == 0 || vetor[i] != vetor[i-1]){
+            (*vetorSegmentos)[tamVetorSegmentos] = buscaSegmentoDoNum(vetorSimplificado, vetor[i], tam);
             tamVetorSegmentos++;
         }
     }
 
-    vetorSegmentos = realloc(vetorSegmentos, tamVetorSegmentos * sizeof(int));
-    return vetorSegmentos;
+    *vetorSegmentos = realloc(*vetorSegmentos, tamVetorSegmentos * sizeof(int));
+}
+
+void criaVetorNElementos(int vetor[], int **vetorNElementos, int N){
+    *vetorNElementos = calloc(N, sizeof(int));
+    int tamVetorNElementos=0;
+
+    for(int i=1; i<N; i++){
+        if(vetor[i] == vetor[i-1]){
+            (*vetorNElementos)[tamVetorNElementos]++;
+        } else
+            tamVetorNElementos++;
+    }
+
+    *vetorNElementos = realloc(*vetorNElementos, tamVetorNElementos * sizeof(int));
+}
+
+void criaMatriz(int ***matriz, int *vetorSegmentos, int *vetorNElementos, int numSegmentos){
+    *matriz = malloc(2 * sizeof(int*));
+    
+    for(int i = 0; i < 2; i++){
+        (*matriz)[i] = malloc(numSegmentos * sizeof(int));
+    }
+
+    for(int i = 0; i < numSegmentos; i++){
+        (*matriz)[0][i] = vetorSegmentos[i];
+    }
+
+    for(int i = 0; i < numSegmentos; i++){
+        (*matriz)[1][i] = vetorNElementos[i];
+    }
 }
 
 int main(){
@@ -74,26 +102,47 @@ int main(){
     }
 
     int *vetorOrdenado = malloc(N*sizeof(int));
-
     ordenaVetor(vetor, N, vetorOrdenado);
 
-    for(int i=0; i<N; i++){
-        printf("%d ", vetorOrdenado[i]);
-    }
-
-    printf("\n\n");
-
     int numSegmentos = contaSegmentos(vetor, N);
-
     int *tam = malloc(sizeof(int));
+    int *vetorSimplificado, *vetorSegmentos, *vetorNElementos;
+    int **matriz;
 
-    int *vetorSimplificado = simplificaVetor(vetorOrdenado, N, tam);
-    int *vetorSegmentos = criaVetorSegmentos(vetor, vetorSimplificado, tam, N);
+    simplificaVetor(vetorOrdenado, &vetorSimplificado, tam, N);
+    criaVetorSegmentos(vetor, vetorSimplificado, &vetorSegmentos, *tam, N);
+    criaVetorNElementos(vetor, &vetorNElementos, N);
+    criaMatriz(&matriz, vetorSegmentos, vetorNElementos, numSegmentos);
 
-    for(int i=0; i<*tam; i++){
-        printf("%d ", vetorSimplificado[i]);
+    for(int i=0; i<numSegmentos; i++){
+        if(i==numSegmentos-1){
+            printf("%d", matriz[0][i]);
+        } else
+            printf("%d ", matriz[0][i]);
     }
 
+    printf("\n");
+
+    for(int i=0; i<numSegmentos; i++){
+        if(i==numSegmentos-1){
+            printf("%d", matriz[1][i] + 1);
+        } else
+            printf("%d ", matriz[1][i] + 1);
+    }
+
+    printf("\n");
+
+
+    for(int i = 0; i < 2; i++){
+        free(matriz[i]);
+    }
+    free(matriz);
+
+    free(vetorOrdenado);
+    free(vetorSimplificado);
+    free(vetorSegmentos);
+    free(vetorNElementos);
+    free(tam);
 
     return 0;
 }
