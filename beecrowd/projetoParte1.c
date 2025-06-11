1e0c8e6033c6ff697154086042e185b66ffd05dd
matheusd-ramos <matheusd.araujo5@gmail.com>
2025-06-11 08:42:40 -0300

    Adição de funções da Lista

--- beecrowd/projetoParte1.c
+++ beecrowd/projetoParte1.c
@@ -7,6 +7,56 @@ Data: 13/06/2025
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INICIOARRANJO 1
+#define MAXTAM 1000
+
+typedef int TipoApontador;
+typedef int TipoChave;
+
+typedef struct{
+    TipoChave Chave;
+    int TipoSegmento;
+    int NumElementos;
+    int PontoMedio;
+} Segmento;
+
+typedef struct{
+    Segmento Segmentos[MAXTAM];
+    TipoApontador Primeiro, Ultimo;
+} TipoLista;
+
+void FLVazia(TipoLista *Lista){
+    Lista->Primeiro = INICIOARRANJO;
+    Lista->Ultimo = Lista->Primeiro;
+}
+
+int Vazia(TipoLista *Lista){
+    return(Lista->Primeiro==Lista->Ultimo);
+}
+
+void Insere(Segmento x, TipoLista *Lista){
+    if(Lista->Ultimo > MAXTAM){
+        printf("Lista cheia\n");
+    } else {
+        Lista->Segmentos[Lista->Ultimo - 1] = x;
+        Lista->Ultimo++;
+    }
+}
+
+void Retira(TipoApontador p, TipoLista *Lista, Segmento *seg){
+    int aux;
+    if(Vazia(Lista) || p >= Lista->Ultimo){
+        printf("Erro: posicao nao existe\n");
+        return;
+    }
+
+    *seg = Lista->Segmentos[p-1];
+    Lista->Ultimo--;
+    for(aux=p; aux < Lista->Ultimo; aux++){
+        Lista->Segmentos[aux-1] = Lista->Segmentos[aux];
+    }
+}
+
 /* Fiz uma função para ler a entrada e, ao mesmo tempo, criar um vetor simplificado 
    (vetor com os números sem repetição e na mesma ordem) e retornar seu tamanho */
 int lerEntrada(int N, int vetor[], int **vetSimp){
@@ -34,6 +84,8 @@ int lerEntrada(int N, int vetor[], int **vetSimp){
     return j + 1; // Retorna o tamanho do vetor simplificado
 }
 
+/* Essa função une a ordenação dos vetores com a criação de um vetor de mapeamento,
+   ao invés de ter uma função para cada uma dessas etapas*/
 void criarVetSeg(int tamVetSimp, int *vetSimp, int **vetSeg) {
     *vetSeg = (int*)malloc(tamVetSimp * sizeof(int));
     
@@ -80,25 +132,35 @@ void criarVetSeg(int tamVetSimp, int *vetSimp, int **vetSeg) {
     free(valoresUnicos);
 }
 
+int verificaSeg(int *vetSeg, int tamVetSimp, int sequencia[]){
+    int cont=0;
+    for(int i=0; i<tamVetSimp-5; i++){
+        cont=0;
+        for(int j=0; j<5; j++){
+            if(vetSeg[i+j] == sequencia[j])
+                cont++;
+        }
+        if(cont == 5)
+            return 1;
+    }
+    return 0;
+}
+
 int main(){
     int N;
     scanf("%d", &N);
     
     int vetor[N];  
+    int sequencia[] = {1, 3, 2, 3, 1};
     int *vetSimp, *vetSeg;
     int tamVetSimp = lerEntrada(N, vetor, &vetSimp);
 
     criarVetSeg(tamVetSimp, vetSimp, &vetSeg);
     
-    for(int i = 0; i < tamVetSimp; i++){
-        printf("%d ", vetSimp[i]);
-    }
-
-    printf("\n");
-
-    for(int i = 0; i < tamVetSimp; i++){
-        printf("%d ", vetSeg[i]);
-    }
+    if(verificaSeg(vetSeg, tamVetSimp, sequencia)){
+        printf("Resultado: Padrao encontrado.\n");
+    } else  
+        printf("Resultado: Padrao nao encontrado.\n");
     
     // Libera a memória alocada
     free(vetSimp);
